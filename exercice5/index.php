94d45876a9fa8c07b4fbcece06f0b86435990030
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-04 12:24:09 +0200

    exo5 et 6

---
+++ exercice5/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo5</title>
+</head>
+
+<body>
+    <h1>Exercice 5</h1>
+
+    <p>Créer une variable de type int. L'initialiser avec <b>rien</b>. Afficher sa valeur.  <br>
+Donner une valeur à cette variable et l'afficher.</p>
+
+
+    <?php
+$int = null;
+
+echo $int;
+
+$int = 42;
+
+echo $int;
+
+?>
+
+</body>
+
+</html>
---
+++ exercice6/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo6</title>
+</head>
+
+<body>
+    <h1>Exercice 6</h1>
+
+    <p>Créer une variable name et l'initialiser avec la valeur de votre choix.  <br>
+    Afficher : <i>Bonjour </i>+ <b>name</b> + <i>, comment vas tu ?</i>.</p>
+
+
+    <?php
+
+$name = "Anthony";
+
+echo "<i>Bonjour </i><b>$name</b><i>, comment vas tu ?</i>";
+
+?>
+
+</body>
+
+</html>
