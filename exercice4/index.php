d65ed9a749ff9e11af8b4cba49e59a5ec7aeaa01
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-04 12:02:05 +0200

    exo4

---
+++ exercice4/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo4</title>
+</head>
+
+<body>
+    <h1>Exercice 4</h1>
+
+    <p>Créer une variable de type string, une variable de type int, une variable de type float, 
+    une variable de type booléan et les initialiser avec une valeur de votre choix.  
+Les afficher.</p>
+
+
+    <?php
+$varstring = "pouet";
+var_dump($varstring);
+
+$varint = 42;
+var_dump($varint);
+
+$varfloat = 3.1415;
+var_dump($varfloat);
+
+$varboolean = true;
+var_dump($varboolean);
+
+?>
+
+</body>
+
+</html>
