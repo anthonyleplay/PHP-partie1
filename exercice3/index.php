638b904839fa5c8636fba3ee6fd042e34d2d3ab1
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-04 11:49:24 +0200

    exo2

--- exercice3/index.php
+++ exercice3/index.php
@@ -4,21 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exo2</title>
+    <title>exo3</title>
 </head>
 
 <body>
-    <h1>Exercice 2</h1>
+    <h1>Exercice 3</h1>
 
-    <p>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix.
-    Attention age est de type entier.  Afficher leur contenu.</p>
+    <p>Créer une variable km. L'initialiser à 1. Afficher son contenu. <br>
+        Changer sa valeur par 3. Afficher son contenu. <br>
+        Changer sa valeur par 125. Afficher son contenu.</p>
 
 
-<?php
-$lastname = "Le Play";
-$firstname = "Anthony";
-$age = 33;
-echo "Bonjour je m'appelle ", $lastname, " ", $firstname, " et j'ai ", $age, " ans";
+    <?php
+$km = 1;
+echo $km,"<br>";
+$km = 3;
+echo $km,"<br>";
+$km = 125;
+echo $km,"<br>";
 ?>
 
 </body>
