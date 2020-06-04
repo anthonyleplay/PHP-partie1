d19db6d1751b970bcb8f6fa0ceec806c08ac2929
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-04 11:45:27 +0200

    exo2

---
+++ exercice2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo2</title>
+</head>
+
+<body>
+    <h1>Exercice 2</h1>
+
+    <p>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix.
+    Attention age est de type entier.  Afficher leur contenu.</p>
+
+
+<?php
+$lastname = "Le Play";
+$firstname = "Anthony";
+$age = 33;
+echo "Bonjour je m'appelle ", $lastname, " ", $firstname, " et j'ai ", $age, " ans";
+?>
+
+</body>
+
+</html>
